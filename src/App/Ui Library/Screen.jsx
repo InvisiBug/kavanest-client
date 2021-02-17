660e36654e5114569729a4f736f43eccc41a828f
InvisiBug <matthewjkavanagh@gmail.com>
2021-02-17 22:23:07 +0000

    Cleared out code

--- src/App/Ui Library/Screen.jsx
+++
@@ -1,6 +0,0 @@
-/* Make this the screen container, 
-remember last night when i decided to make the 
-max screen width a specific size 
-and thought it would be better to do it in all places at once
-
-*/
