ccf6b433efd003a29b13d2a151f6ef8b753f7721
InvisiBug <matthewjkavanagh@gmail.com>
2021-02-18 21:04:50 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import ReactDOM from "react-dom";
 import App from "./App/App.tsx";
 
 // CSS
-// import "./CSS/Computer.css";
-// import "./CSS/Climate.css";
-// import "./CSS/Graphs.css";
-// import "./CSS/Button.css";
-// import "./CSS/Lights.css"; // deffo needed
+import "./CSS/Computer.css";
+import "./CSS/Climate.css";
+import "./CSS/Graphs.css";
+import "./CSS/Button.css";
+import "./CSS/Lights.css"; // deffo needed
 
 // Render to page
 ReactDOM.render(<App />, document.getElementById("root")); // Change this back to router if needed
