afd180b68bb20fad53970b51bae7c7bacea964a1
InvisiBug <matthewjkavanagh@gmail.com>
2024-11-24 19:51:28 +0000

    deploying vite upgrade v10

--- vite.config.ts
+++ vite.config.ts
@@ -31,22 +31,10 @@ export default defineConfig(({ command, mode }) => {
       VitePWA({
         includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
         manifest: {
-          name: "My Awesome App",
-          short_name: "MyApp",
-          description: "My Awesome App description",
+          name: "KavaNest Control Interface",
+          short_name: "Kavanest",
           theme_color: "#ffffff",
-          icons: [
-            {
-              src: "pwa-192x192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "pwa-512x512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-          ],
+          start_url: ".",
           display: "standalone",
         },
       }),
