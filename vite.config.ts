3161cad112f37a55fdec41ed91177cb7c4037eeb
InvisiBug <matthewjkavanagh@gmail.com>
2024-11-24 19:44:08 +0000

    deploying vite upgrade v10

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+
+import fs from "fs";
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -23,7 +26,40 @@ import path from "path";
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: "My Awesome App",
+          short_name: "MyApp",
+          description: "My Awesome App description",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+          display: "standalone",
+        },
+      }),
+      // {
+      //   name: "postbuild-commands",
+      //   closeBundle: () => {
+      //     const path = "./dist/manifest.json";
+      //     const manifest = JSON.parse(fs.readFileSync(path).toString());
+      //     manifest["display"].customProp = "standalone";
+      //     fs.writeFileSync(path, JSON.stringify(manifest, null, 2));
+      //   },
+      // },
+    ],
     resolve: {
       alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
