c87f6b47364b3cf05ff96ea2bb7d0b3cbca0ee96
InvisiBug <matthewjkavanagh@gmail.com>
2024-11-27 13:36:34 +0000

    Fix keyboard closing issue

--- src/lib/helpers/useWindow.ts
+++ src/lib/helpers/useWindow.ts
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
 
+/**
+ * Be careful using this one.
+ * If its included in a component and you've called the hooke (even if you're not using the output)
+ * re renders will occur whenever the keyboard opens
+ * causing the keyboard to close immediately
+ * Urgh :(
+ */
 const useWindowDimensions = () => {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
