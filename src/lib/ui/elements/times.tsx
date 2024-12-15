b03ebe1875167c0b0f845d40bb519d2b5a35e402
InvisiBug <matthewjkavanagh@gmail.com>
2024-12-15 23:30:06 +0000

    updated override sizes

--- src/lib/ui/elements/times.tsx
+++ src/lib/ui/elements/times.tsx
@@ -33,7 +33,7 @@ export default Times;
 type Props = {
   updateTimer: (time: number) => void;
   times?: number[];
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const borders = false;
--- src/lib/ui/thermometer/index.tsx
+++ src/lib/ui/thermometer/index.tsx
@@ -5,28 +5,29 @@ import { sketch } from "./sketch";
 import styled from "@emotion/styled";
 import { mq, px } from "@/lib/mediaQueries";
 
-const Thermometer: FC<any> = ({ temp = 10, set }) => {
-  const [currentTemp, setCurrentTemp] = useState<number>(temp);
-  console.log("🚀 ~ currentTemp:", currentTemp);
-  const [setpoint, setSetpoint] = useState<number>(set);
-  const { height, width } = useWindowDimensions();
+const Thermometer: FC<any> = ({ temp, set }) => {
+  // const [currentTemp, setCurrentTemp] = useState<number>(temp);
+  // console.log("🚀 ~ currentTemp:", currentTemp);
+  // const [setpoint, setSetpoint] = useState<number>(set);
+  // const { height, width } = useWindowDimensions();
 
-  console.log(setpoint);
-  useEffect(() => {
-    // console.log(width <= parseInt(px("large")));
-    const interval = setInterval(() => setCurrentTemp((currentTemp) => currentTemp + 0.5), 100);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [width]);
+  // console.log(setpoint);
+  // useEffect(() => {
+  //   // console.log(width <= parseInt(px("large")));
+  //   const interval = setInterval(() => setCurrentTemp((currentTemp) => currentTemp + 0.5), 100);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [width]);
 
   // if (width <= parseInt(String(px("large")))) return <></>;
+  console.log(temp);
 
   return (
     <>
       <h1>hello</h1>
       <ThermometerContainer>
-        <ReactP5Wrapper sketch={sketch} currentTemp={currentTemp} target={15} deadzone={2} set={setpoint} />
+        <ReactP5Wrapper sketch={sketch} currentTemp={temp} target={15} deadzone={2} set={set} />
       </ThermometerContainer>
     </>
   );
@@ -43,27 +44,3 @@ const ThermometerContainer = styled.div`
 `;
 
 export default Thermometer;
-
-const getWindowDimensions = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-
-  return {
-    width,
-    height,
-  };
-};
-
-const useWindowDimensions = () => {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return windowDimensions;
-};
